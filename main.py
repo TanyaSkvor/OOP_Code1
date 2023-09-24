ee7a79e0a86f7c6481e751912dae74f3f87dd813
Tanya Skvortsova <tanyusha.skvortsova98@mail.ru>
2023-09-24 21:21:03 +0300

    task 2

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def lect_grd(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress::
+            if course in lecturer.lecture_grades:
+                lecturer.lecture_grades[course] += [grade]
+            else:
+                lecturer.lecture_grades[course] = [grade]
+        else:
+            return 'Ошибка'
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -14,6 +22,15 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
-
+    def __init__(self):
+        self.lecture_grades = {}
+        
 class Reviewer(Mentor):
-
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
